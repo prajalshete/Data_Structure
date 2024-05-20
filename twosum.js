7aa0b4a36d3bc6127bef8fb5cf3d90cb93a01993
64058_prajal <prajalshete@gmail.com>
2024-05-20 19:54:35 +0530

    twosum program added

---
+++ twosum.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums, target) {
+    for(i=0;i<=nums.length;i++){
+        for(j=i+1;j<=nums.length;j++){
+            if(target===nums[i]+nums[j]){
+                return[i,j]
+            }
+        }
+    }
+};
