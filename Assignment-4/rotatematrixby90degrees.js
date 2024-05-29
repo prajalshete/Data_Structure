148c7067c46116c9eb9fc6a04ce46ada559f9b0d
64058_prajal <prajalshete@gmail.com>
2024-05-29 22:46:33 +0530

    Rotatearrayby90degrees program added

---
+++ Assignment-4/rotatematrixby90degrees.js
@@ -0,0 +1,31 @@
+function rotatearray( array) {
+    const n =  array.length;
+    
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [ array[i][j],  array[j][i]] = [ array[j][i],  array[i][j]];
+        }
+    }
+    
+   
+    for (let i = 0; i < n; i++) {
+        array[i].reverse();
+    }
+    
+    return  array;
+}
+
+
+let array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+
+
+let rotatedarray = rotatearray( array);
+
+
+console.log(rotatedarray);
