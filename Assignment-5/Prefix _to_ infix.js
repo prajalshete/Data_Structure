528b3c894aefb9f41e1e102e812275f6c9283314
64058_prajal <prajalshete@gmail.com>
2024-06-06 00:47:33 +0530

    prefix to infix program is added

---
+++ Assignment-5/Prefix _to_ infix.js
@@ -0,0 +1,26 @@
+function isOperator(char) {
+    return char === '+' || char === '-' || char === '*' || char === '/';
+}
+
+function prefixToInfix(prefix) {
+    const stack = [];
+
+    for (let i = prefix.length - 1; i >= 0; i--) {
+
+        if (isOperator(prefix[i])) {
+            let operand1 = stack.pop();
+            let operand2 = stack.pop();
+            let infixExp = `(${operand1}${prefix[i]}${operand2})`;
+            stack.push(infixExp);
+        } else {
+            stack.push(prefix[i]);
+        }
+    }
+    
+    return stack.pop();
+}
+
+const prefixExpression = "-+AB*CD";
+const infixExpression = prefixToInfix(prefixExpression);
+console.log("Prefix Expression:", prefixExpression);
+console.log("Infix Expression:", infixExpression);
